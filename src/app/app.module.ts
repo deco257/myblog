b47aa4ef3d4d646170974028015e0648103493a4
deco257 <derickkazikazi@gmail.com>
2023-03-05 11:02:32 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { GalerieComponent } from './galerie/galerie.component';
+import { PubliciteComponent } from './publicite/publicite.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    GalerieComponent,
+    PubliciteComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
